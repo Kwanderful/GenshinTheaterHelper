58c6539d74871d109d18dd683226703ed56d6f05
Kels <kwanderlingh26@gmail.com>
2026-09-04 00:47:14 -0700

    Remove August theater

--- app/utilities/theaters.js
+++ app/utilities/theaters.js
@@ -1,21 +1,15 @@
 const theaters = [
     {
-        month: "August",
-        elements: ["Hydro", "Electro", "Cryo"],
-        opening_cast: ["Yelan", "Aino", "Flins", "Ororon", "Skirk", "Layla"],
-        special_invites: ["Arlecchino", "Chevreuse", "Kaedehara Kazuha", "Tighnari"],
+        month: "September",
+        elements: ["Hydro", "Electro", "Dendro"],
+        opening_cast: ["Columbina", "Xingqiu", "Cyno", "Kuki Shinobu", "Lauma", "Kaveh"],
+        special_invites: ["Nicole", "Sandrone", "Sucrose", "Odette"],
         act_3_boss: "",
         act_6_boss: "",
         act_8_boss: "",
         act_10_boss: "",
         arcana_challenge_1: [],
         arcana_challenge_2: []
-    },
-    {
-        month: "September",
-        elements: ["Hydro", "Electro", "Dendro"],
-        opening_cast: ["Columbina", "Xingqiu", "Cyno", "Kuki Shinobu", "Lauma", "Kaveh"],
-        special_invites: ["Nicole", "Sandrone", "Sucrose", "Odette"],
     },
     {
         month: "October",
